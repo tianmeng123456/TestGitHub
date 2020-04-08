cf8587b862fc33074eee72312687ac67e9b8065a
tianmeng <1311464746@qq.com>
2020-04-08 18:44:16 +0800

    这是我在版本一进行修改的代码

--- test-git/src/main/java/com/ittm/TestGit/TestGit.java
+++ test-git/src/main/java/com/ittm/TestGit/TestGit.java
@@ -3,5 +3,6 @@ package com.ittm.TestGit;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("测试开始");
+        System.out.println("这是我在版本一下载的进行修改");
     }
 }
