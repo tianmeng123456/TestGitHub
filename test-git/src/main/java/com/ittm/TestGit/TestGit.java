dcac8f5c404b960fff95183d6f292045bcd6401b
tianmeng <1311464746@qq.com>
2020-04-08 18:59:11 +0800

    这是张三的第六次修改

--- test-git/src/main/java/com/ittm/TestGit/TestGit.java
+++ test-git/src/main/java/com/ittm/TestGit/TestGit.java
@@ -8,5 +8,6 @@ public class TestGit {
         System.out.println("这是版本三修改的代码");
         System.out.println("这是张三的第四次修改");
         System.out.println("这是张三修改的第五次代码");
+        System.out.println("这是张三修改的第六次代码");
     }
 }
